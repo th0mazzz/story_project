fa78fa6f27aa54853c41b328643cd0f68dad26ff
Stuy-Kevin-Lin <klin14@stuy.edu>
2018-10-17 12:29:40 -0400

    Small edit

--- util/setupDB.py
+++ util/setupDB.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-open("../data/info.db","w").close() #Resets Database
+#open("../data/info.db","w").close() #Resets Database
 
 db = sqlite3.connect("../data/info.db")
 
 c = db.cursor()
 
 #Setting up tables
-c.execute("CREATE TABLE userInfo(id INTEGER PRIMARY KEY, username TEXT UNIQUE, pass TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS userInfo(id INTEGER PRIMARY KEY, username TEXT UNIQUE, pass TEXT)")
 
-c.execute("CREATE TABLE stories(name TEXT, username TEXT, contrib TEXT)")
+c.execute("CREATE TABLE IF NOT EXISTS stories(name TEXT, username TEXT, contrib TEXT)")
