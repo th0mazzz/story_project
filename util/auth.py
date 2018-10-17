7ca3fc25fd08457289689666704598eee7ff4c0e
Stuy-Kevin-Lin <klin14@stuy.edu>
2018-10-17 17:41:11 -0400

    Added checkInfo

--- util/auth.py
+++ util/auth.py
@@ -1,6 +1,14 @@
 from flask import Flask
 import sqlite3
 
-db = sqlite3.connect("info")
+db = sqlite3.connect("../data/info.db")
 
 c = db.cursor()
+
+def checkInfo(user,pswd):
+    #Looks for the password of the inputted user
+    for i in c.execute("SELECT pass FROM userInfo WHERE username = ?",(user,)):
+        if i[0] == pswd: return "Login Successful" #If user is found and passwords match
+        else: return "Incorrect Password" #If passwords don't match
+    else:
+        return "User not found" #If the user doesn't exist in the table
