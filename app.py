e8183a1973f8e353b7f836a6dd082b6323047825
Stuy-Kevin-Lin <klin14@stuy.edu>
2018-10-19 12:05:12 -0400

    Re-routed /create

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 #Team SKAR - Sophia Xia, Kevin Lin, Aaron Li, Ricky Lin
 
 import util
-from flask import Flask
+from flask import Flask, request, render_template, session, url_for, redirect, flash
 app = Flask(__name__) #Create instance of class Flask
 
 @app.route("/") #Assign fxn to route
-def hello_world():
-    return "No hablo queso!"
+def index():
+    return render_template("index.html")
 
-@app.route("/auth"):
+@app.route("/create")
+def create():
+    return render_template("create.html")
+
+@app.route("/auth")
 def auth():
     return "Temp"
 
