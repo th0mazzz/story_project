1f67da3fc2f7443a43ef8c2a3d54d4ceaa302335
Stuy-Kevin-Lin <klin14@stuy.edu>
2018-10-21 21:21:53 -0400

    Templates, sessions, and redirects

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from util import auth
 from flask import Flask, request, render_template, session, url_for, redirect, flash
 app = Flask(__name__) #Create instance of class Flask
 
+app.secret_key = 'supersecure'
 
 @app.route("/") #Assign fxn to route
 def index():
+    if 'user' in session.keys():
+        return redirect('/profile')
     return render_template("index.html")
 
 @app.route("/create")
@@ -15,9 +18,23 @@ def create():
 
 @app.route("/auth",methods = ['POST'])
 def authenticate():
+    loginStatus = ''
+    if not('user' in request.form.keys()):
+        redirect('/')
     if "pass2" in request.form.keys():
-        return auth.createAcc(request.form['user'],request.form['pass1'],request.form['pass2'])
-    return auth.checkInfo(request.form["user"],request.form["pass"])
+        loginStatus =  auth.createAcc(request.form['user'],request.form['pass1'],request.form['pass2'])
+    else: loginStatus = auth.checkInfo(request.form["user"],request.form["pass"])
+    if loginStatus in ["Account creation successful","Login Successful"]:
+        session['user'] = request.form['user']
+        return redirect('/profile')
+    else:
+        flash(loginStatus)
+        #Redirects to previous page or root if there is none
+        redirect(request.referrer or '/')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',user = session['user'])
 
 @app.route("/forbidden")
 def forbidden():
