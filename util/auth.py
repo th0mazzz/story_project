5b62fe76e624c317cb48aa1560b62479c4969db3
unknown <rlin6@stuy.edu>
2018-10-23 21:42:48 -0400

    fixed database error

--- util/auth.py
+++ util/auth.py
@@ -1,31 +1,38 @@
 from flask import Flask
 import sqlite3
 
-db = sqlite3.connect("data/info.db",check_same_thread = False)
-
-c = db.cursor()
-
 def checkInfo(user,pswd):
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     #Looks for the password of the inputted user
     for i in c.execute("SELECT pass FROM userInfo WHERE username = ?",(user,)):
          #If user is found and passwords match
         if i[0] == pswd: return "Login Successful"
          #If passwords don't match
-        else: return "Incorrect Password"
+        else: 
+            db.close()
+            return "Incorrect Password"
     else:
         #If the user doesn't exist in the table
+        db.close()
         return "User not found"
 
 def createAcc(user,pswd,passConf):
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     #checks if the username already exists
     for i in c.execute("SELECT username FROM userInfo WHERE username = ?",(user,)):
+        db.close()
         return "Username already exists"
     else:
         #if password confirmation fails
-        if pswd != passConf: return "Passwords do not match"        
+        if pswd != passConf: 
+            db.close()
+            return "Passwords do not match"        
         #if password confirmation succeeds add the user to the database
         c.execute("INSERT INTO userInfo (username,pass) VALUES(?,?)",(user,pswd,))
         db.commit()
+        db.close()
         return "Account creation successful"
 
 print(createAcc("abc","123","123")) #expect account creation successful
--- util/story.py
+++ util/story.py
@@ -1,42 +1,52 @@
 from flask import Flask
 import sqlite3
 
-db = sqlite3.connect("data/info.db",check_same_thread=False)
-
-c = db.cursor()
-
 def createStory(title,username,firstLine):
     #checks if title is unique
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     for i in c.execute("SELECT name FROM stories WHERE name = ?", (title,)):
+        db.close()
         return "Title already exists"
     else:
         #inserts given information into the table
         c.execute("INSERT INTO stories (name,username,contrib) VALUES(?,?,?)",(title,username,firstLine,))
         db.commit()
+        db.close()
         return "Successfully created story"
 
 def editStory(title,username,newLine):
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     #checks if the user already made a contribution
     for i in c.execute("SELECT name FROM stories WHERE name = ? AND username = ?",(title,username,)):
+        db.close()
         return "You have already contributed"
     else:
         #inserts given information into the table
         for z in c.execute("SELECT name FROM stories WHERE name = ?", (title,)):
             c.execute("INSERT INTO stories (name,username,contrib) VALUES(?,?,?)",(title,username,newLine,))
             db.commit()
+            db.close()
             return "Successfully added to story"
+        db.close()
         return "Story does not exist"
 
 def getAll():
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     c.execute("SELECT name FROM stories")
     everything = c.fetchall()
     output = set()
     for i in everything:
         output.add(i[0])
     #print(output)
+    db.close()
     return(output)
     
 def getStories(username):
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     #selects all the stories the user has contributed to
     c.execute("SELECT name FROM stories WHERE username = ?", (username,))
     rows = c.fetchall()
@@ -44,35 +54,50 @@ def getStories(username):
     for i in rows:
         output.add(i[0])
     #print(output)
+    db.close()
     return(output)
 
 def getUndiscovered(username):
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     output = getAll() - getStories(username)
-    return output;
+    db.close()
+    return output
     #print(output)
 
 def getLast(storyname):
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     #Selects latest entry of the story
     for i in c.execute("SELECT contrib FROM stories WHERE name = ? ORDER BY ROWID DESC LIMIT 1;",(storyname,)):
         #Return latest entry if found
+        db.close()
         return [i[0]]
     else:
         #Returned if no entry found
+        db.close()
         return ["Story does not exist"]
 
 def getDiscoverDict(username):
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     output = dict()
     undisList = getUndiscovered(username)
     for i in undisList:
         output[i] = getLast(i)[0]
+    db.close()
     return output
     
 def getFull(storyname):
+    db = sqlite3.connect("data/info.db")
+    c = db.cursor()
     output = []
     for i in c.execute("SELECT contrib FROM stories WHERE name = ? ORDER BY ROWID;",(storyname,)):
         output.append(i[0]) #Adds all entries of a story in order of insertion to an output string
     if output == []:
+        db.close()
         return ['Story does not exist'] #Returned if no story found
+    db.close()
     return output
 
 #diagnostic print statements
